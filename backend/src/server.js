8d9ddeb1cb85bee2c7bc0a47a08ccd661e3a7ba0
SahuUsha <sahuur@rknec.edu>
2025-08-30 18:53:31 +0530

    chnage

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,8 @@ const app = express()
 app.use(cors({
     origin: [
         // 'https://streamtube-rjpiz2tco-sahuushas-projects.vercel.app', // your Vercel frontend
-        "https://streamtube-b08xqxs2l-sahuushas-projects.vercel.app",
+        // "https://streamtube-b08xqxs2l-sahuushas-projects.vercel.app",
+        "https://streamtube-27by25m69-sahuushas-projects.vercel.app",
         'http://localhost:5173' // for local testing
     ],
     credentials: true, // allow cookies
